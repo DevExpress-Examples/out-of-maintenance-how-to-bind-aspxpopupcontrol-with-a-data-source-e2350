752d8086354f92f71f7505702c25d5fe43877224
DevExpressExampleBot <fakeEmail>
2022-12-07 15:52:31 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -2,7 +2,7 @@ using System;
 using System.Data;
 
 public partial class _Default : System.Web.UI.Page {
-    protected void ASPxPopupControl1_WindowDataBound(object source, DevExpress.Web.ASPxPopupControl.PopupWindowEventArgs e) {
+    protected void ASPxPopupControl1_WindowDataBound(object source, DevExpress.Web.PopupWindowEventArgs e) {
         DataRowView dataItem = (DataRowView)e.Window.DataItem;
         e.Window.Modal = Convert.ToBoolean(dataItem["Modal"]);
     }
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -4,7 +4,7 @@ Imports System.Data
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
-	Protected Sub ASPxPopupControl1_WindowDataBound(ByVal source As Object, ByVal e As DevExpress.Web.ASPxPopupControl.PopupWindowEventArgs)
+	Protected Sub ASPxPopupControl1_WindowDataBound(ByVal source As Object, ByVal e As DevExpress.Web.PopupWindowEventArgs)
 		Dim dataItem As DataRowView = CType(e.Window.DataItem, DataRowView)
 		e.Window.Modal = Convert.ToBoolean(dataItem("Modal"))
 	End Sub
