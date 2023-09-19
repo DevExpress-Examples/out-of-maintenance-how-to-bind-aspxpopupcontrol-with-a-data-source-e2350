0a670a9d542812575d8c25d649980c2b112f1ed7
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 13:12:55 +0400

    Change example status to out of maintenance

--- CS/WebSite/Default.aspx.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Data;
-
-public partial class _Default : System.Web.UI.Page {
-    protected void ASPxPopupControl1_WindowDataBound(object source, DevExpress.Web.PopupWindowEventArgs e) {
-        DataRowView dataItem = (DataRowView)e.Window.DataItem;
-        e.Window.Modal = Convert.ToBoolean(dataItem["Modal"]);
-    }
-}
